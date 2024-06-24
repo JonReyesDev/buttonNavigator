be247bee6cb6d6ff3fed907282a763fa6503364d
Jon Reyes <jonath.lt3@gmail.com>
2024-06-24 14:07:33 -0700

    Se creo el diseño

---
+++ app/src/main/java/com/example/appmenubutton/MainActivity.kt
@@ -0,0 +1,59 @@
+package com.example.appmenubutton
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var bottomNavigationView: BottomNavigationView
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        // init
+        init();
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+    private fun init() {
+        bottomNavigationView = findViewById(R.id.btnNavegator)
+        changeFrame(HomeFragment())
+        bottomNavigationView.setOnItemSelectedListener {
+            menuItem ->
+                when(menuItem.itemId) {
+                    R.id.btnHome -> {
+                        changeFrame(HomeFragment())
+                        true
+                    }
+                    R.id.btnLista -> {
+                        changeFrame(ListFragment())
+                        true
+                    }
+                    R.id.btnDb -> {
+                        changeFrame(DbFragment())
+                        true
+                    }
+                    R.id.btnAcerca -> {
+                        changeFrame(AboutFragment())
+                        true
+                    }
+                    else -> {
+                        false
+                    }
+                }
+        }
+    }
+    private fun changeFrame(fragment: Fragment) {
+        supportFragmentManager.beginTransaction().replace(R.id.frmContenedor, fragment).commit()
+    }
+}
